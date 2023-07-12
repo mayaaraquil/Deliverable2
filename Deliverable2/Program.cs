c2455184ca4f9813b650b9b245db5b90ff0eda03
Maya Araquil <mayaaraquil775@gmail.com>
2023-07-12 17:37:17 -0400

    Deliverable2

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Serialization;
+
+Console.WriteLine("Welcome to the COIN FLIP CHALLENGE!");
+
+
+Console.WriteLine("What is your name?");
+    string name =Console.ReadLine();
+
+Console.WriteLine("Do you want to do the COIN FLIP CHALLENGE? (Yes/No)");
+string choice = Console.ReadLine();
+
+if (choice.ToLower() == "no")
+{
+    Console.WriteLine("You suck, Coward!");
+    return;
+}
+
+int score = 0;
+
+for  (int i = 0; i <= 5; i++)
+{
+    int randomNum = new Random().Next(2);
+
+    Console.WriteLine($"\nRound {i}; Heads or Tails? ");
+    string guess = Console.ReadLine().ToLower();
+    
+    if ((randomNum == 0 && guess == "heads") || (randomNum == 1 && guess == "tails"))
+    {
+        score++;
+        Console.WriteLine("Correct!");
+    }
+    else
+    {
+        Console.WriteLine("HA! Incorrect!");
+    }
+}
+
+Console.WriteLine($"\n{name}, your total score is: {score}");
